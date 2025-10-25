ecf22309ba6726f7f0303903a5111ca627c14dd0
Pawnpawnn <67137119+Pawnpawnn@users.noreply.github.com>
2025-10-25 11:58:51 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1767,7 +1767,7 @@ local function updateEventButtons()
 
     -- Hapus button lama
     for _, b in pairs(eventButtons) do
-        b:Remove()
+        b:Set()
     end
     table.clear(eventButtons)
 
